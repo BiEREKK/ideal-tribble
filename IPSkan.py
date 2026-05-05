5b292fe16dc230f1741921ce6c9516354e20b4ae
BiEREKK <kbernat2008@gmail.com>
2026-05-05 11:28:09 +0200

    Add files via upload

---
+++ IPSkan.py
@@ -0,0 +1,23 @@
+import socket
+import ipaddress
+import os
+
+def get_network(): 
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    finally:
+            s.close()
+    return ipaddress.IPv4Network(ip + "/24", strict=False)
+
+def ping(ip):
+    return os.system(f"ping -c 1 -w 1 {ip} > /dev/null 2>&1") == 0
+
+net = get_network()
+print ("Skanowanie: {net}\n")
+
+for ip in net.hosts():
+    ip = str(ip)
+    if ping(ip):
+        print(ip)
